4c92894838d265a21ceffa02f6d5291c45341c06
FCaivan0 <fcaivano@innorg.it>
2024-04-03 11:45:19 +0200

    first commit of the day

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Drawer component TEMP
+import { Drawer } from 'react-native-paper';
+
+//header component TEMP
+import { Appbar } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+//query page TEMP
+import { Card, Button } from 'react-native-paper';
+import { ScrollView } from 'react-native-web';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.safeareaproviderContainer}>
+      <View style={styles.outerContainer}>
+        <View style={styles.drawer}>
+          <Drawer.Item
+            style={{ backgroundColor: '#b5b5b5' }}
+            icon="star"
+            label="First Item"
+          />
+          <Drawer.Item
+            style={{ backgroundColor: '#b5b5b5' }}
+            icon="heart"
+            label="Second Item"
+          />
+        </View>
+        <View style={styles.container}>
+          <Appbar.Header style={styles.header}>
+            <Appbar.Content title="Title" titleStyle={{ color: '#fff' }} />
+          </Appbar.Header>
+          <Card>
+            <Card.Title title="Seleziona visualizzazione" />
+            <Card.Content>
+              <Button>Task chiusi</Button>
+            </Card.Content>
+            <Card.Actions>
+              <Button>Visualizza</Button>
+              <Button>Cancella tutto</Button>
+            </Card.Actions>
+          </Card>
+          <ScrollView>
+
+          </ScrollView>
+        </View>
+      </View>
+    </SafeAreaProvider>
+
   );
 }
 
@@ -14,7 +56,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  drawer: {
+    width: 230,
+    backgroundColor: '#b5b5b5',
+    // position: 'fixed',
+    //height: '100%',
+    minHeight: '100%',
+  },
+  outerContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  safeareaproviderContainer: {
+    flex: 1
+  },
+  header: {
+    backgroundColor: '#E7E0EC',
   },
 });
